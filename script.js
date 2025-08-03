6b194c7d1eb7817f05c4022d3e1c0017ef6dbfbd
Kavyaav12 <kavyaav100@gmail.com>
2025-08-03 17:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
-  // write your code here
+ let name="hello world";
+	console.log(name.length);
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+//const str = prompt("Enter some string.");
+//alert(byteSize(str));
